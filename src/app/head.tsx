03e6b5ca10e894ec26d6e743b166f6e9e3c3de5d
godMrwang <1457029580@qq.com>
2025-08-28 01:43:57 +0800

    添加 Google 验证 meta 标签

---
+++ src/app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <title>我的博客</title>
+      <meta name="google-site-verification" content="HpRZvaRYZEd3eS9YEOM3t5TR8zG0tHRSUO12KSZftn8" />
+    </>
+  )
+}

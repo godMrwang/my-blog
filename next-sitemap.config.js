fba150cdd6a400990930bfa10f22c64904e2fd99
godMrwang <1457029580@qq.com>
2025-08-29 17:08:23 +0800

    添加 next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://my-blog-pi-dun.vercel.app',
+  generateRobotsTxt: true,
+  changefreq: 'weekly',
+  priority: 0.7,
+};
